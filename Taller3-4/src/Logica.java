d6f8054a2025a8feee11dfe1528d4d990cbd1cc8
Alitza <alitza-konata.13@hotmail.com>
2016-11-03 08:01:35 -0500

    comentario

--- Taller3-4/src/Logica.java
+++ Taller3-4/src/Logica.java
@@ -13,7 +13,11 @@ public class Logica {
 		this.sp = new SistemaParticula(new PVector(app.width/2, 50), app);
 	}
 	
+	/**
+	 * este metodo se encarga de pintar en el lienzo
+	 */
 	public void pintar() {
+		
 		sp.addParticle();
 		sp.run();
 	}
